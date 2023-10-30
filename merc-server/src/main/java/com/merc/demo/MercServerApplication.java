0544c609adbfb812b1a0f8e8819c83a71f74c42f
Chauhan <prachau@apac.corpdir.net>
2023-10-30 09:29:32 +0530

    Initial Commit

---
+++ merc-server/src/main/java/com/merc/demo/MercServerApplication.java
@@ -0,0 +1,13 @@
+package com.merc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercServerApplication.class, args);
+	}
+
+}
